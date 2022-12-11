2986b11039aeefb714b7804e8828aaddce11a3d9
Daniil Shipilin <daniil.shipilin@gmail.com>
2022-12-11 18:11:38 +0100

    Nuget packages updated

--- Calculator/Helpers/CurrencyConverter.cs
+++ Calculator/Helpers/CurrencyConverter.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Calculator.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 public static class CurrencyConverter
 {
@@ -45,32 +43,18 @@ public static class CurrencyConverter
             // read response in json format
             string json = await response.Content.ReadAsStringAsync();
 
-            // create string list for possible errors during json processing
-            var errors = new List<string>();
-            var settings = new JsonSerializerSettings()
-            {
-                Error = delegate (object? sender, ErrorEventArgs args)
-                {
-                    // put registered errors in created string list
-                    errors.Add(args.ErrorContext.Error.Message);
-                    args.ErrorContext.Handled = true;
-                }
-            };
-
             // deserialize received json
-            var jsonObj = JsonConvert.DeserializeObject<CurrencyApiJson>(json, settings);
+            var jsonObj = System.Text.Json.JsonSerializer.Deserialize<CurrencyApiJson>(json);
 
-            if (errors.Count > 0)
+            if (jsonObj is null)
             {
-                throw new Exception($"JSON deserialization failed.{Environment.NewLine}" +
-                                    $"{string.Join(Environment.NewLine, errors)}");
+                throw new Exception("JSON deserialization failed");
             }
-            else if (jsonObj != null && !jsonObj.Success && jsonObj.Error != null)
+            else if (!jsonObj.Success && jsonObj.Error is not null)
             {
                 throw new Exception(jsonObj.Error.ToString());
             }
-
-            if (jsonObj != null)
+            else
             {
                 // all checks passed successfully
                 Currencies = jsonObj;
