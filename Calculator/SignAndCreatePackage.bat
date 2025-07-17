d39ec7e2d21b2db6e891adf602d1c7c998054553
Daniil Shipilin <daniil.shipilin@gmail.com>
2025-07-17 16:21:34 +0200

    Updated build scripts

--- Calculator/SignAndCreatePackage.bat
+++ Calculator/SignAndCreatePackage.bat
@@ -4,19 +4,13 @@ set "cert=%CodesignCertPath%"
 set "timestamp=http://timestamp.digicert.com"
 
 set "bin=.\bin"
-set "src=%bin%\Release\publish"
+set "src=%bin%\publish"
 
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\Calculator.exe"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\Calculator.exe"
 
 signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\Calculator.dll"
 signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\Calculator.dll"
-
-signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\ApplicationUpdater.dll"
-signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\ApplicationUpdater.dll"
-
-signtool.exe sign /fd sha256 /a /f "%cert%" "%src%\MathExpressionParser.dll"
-signtool.exe timestamp /tr "%timestamp%" /td sha256 "%src%\MathExpressionParser.dll"
 
 del /S /Q "%bin%\Calculator.zip" >nul 2>&1
 7za.exe a -mx0 -tzip "%bin%\Calculator.zip" "%src%\*"
